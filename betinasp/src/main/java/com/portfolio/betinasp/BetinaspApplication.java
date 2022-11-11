60313f2ab8c42ad77a6cd4f4da56ea73e2c3b3d2
Betina <108528738+betinaspotto@users.noreply.github.com>
2022-11-11 12:03:59 -0300

    nuevo commit

--- betinasp/src/main/java/com/portfolio/betinasp/BetinaspApplication.java
+++ betinasp/src/main/java/com/portfolio/betinasp/BetinaspApplication.java
@@ -2,12 +2,24 @@ package com.portfolio.betinasp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BetinaspApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BetinaspApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BetinaspApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+            }
+        };
+    }
 }
